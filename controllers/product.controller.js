59eba343ce7664964c497a6402b19b6e53d02b9d
José Ignacio Ramón <joseignacioramon24@gmail.com>
2025-07-03 15:48:29 -0300

    Se agregaron las operaciones faltantes en cuanto a productos, asi como sus rutas respectivas

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -7,6 +7,19 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
+// Obtener producto por ID
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id).populate(
+    "category",
+    "name"
+  );
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "Producto no encontrado" });
+  }
+});
+
 // Crear producto (solo admin)
 const createProduct = asyncHandler(async (req, res) => {
   const { name, description, price, category, image } = req.body;
@@ -15,4 +28,38 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct);
 });
 
-export { getProducts, createProduct };
+// Actualizar producto
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, description, price, category, image } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name || product.name;
+    product.description = description || product.description;
+    product.price = price || product.price;
+    product.category = category || product.category;
+    product.image = image || product.image;
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404).json({ message: "Producto no encontrado" });
+  }
+});
+
+// Eliminar producto
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "Producto eliminado" });
+  } else {
+    res.status(404).json({ message: "Producto no encontrado" });
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
